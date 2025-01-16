a300fdb0f3380db730ab553008848b6cfd233555
MudassirAlam7 <mudassiralam7@gmail.com>
2025-01-17 00:20:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,40 @@
 //your JS code here. If required.
-let 
+let title = document.getElementById('title')
+let author = document.getElementById('Author')
+let isbn = document.getElementById('isbn')
+let subbtn = document.getElementById('submit')
+
+let tableBody = document.getElementById('book-list')
+
+subbtn.addEventListener('click', function (e) {
+	e.preventDefault()
+	let Tvalue = title.value.trim()
+	let Avalue = author.value.trim()
+	let Ivalue = isbn.value.trim()
+
+	if(Tvalue == ""|| Avalue =="" || Ivalue ==""){
+		alert('please fill all the field')
+	}
+
+	let newRow = document.createElement('tr')
+	newRow.innerHTML = `<td>${Tvalue}</td>
+	                   <td>${Avalue}</td>
+					   <td>${Ivalue}</td>
+					   <td><button class = "delete">X</button></td>`
+
+	tableBody.append(newRow)
+
+
+	title.value = ""
+	author.value = ''
+	isbn.value = ""
+
+})
+
+tableBody.addEventListener('click', function (e) {
+	if(e.target.classList.contains('delete')){
+		let row = e.target.parentElement.parentElement
+		tableBody.removeChild(row)
+	}
+	
+})
