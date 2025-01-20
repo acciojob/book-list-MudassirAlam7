328b17411593cdc2d90353a8e27d6534f8a8f110
MudassirAlam7 <mudassiralam7@gmail.com>
2025-01-20 22:55:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,40 +1,31 @@
-//your JS code here. If required.
 let title = document.getElementById('title')
-let author = document.getElementById('Author')
+let author = document.getElementById('author')
 let isbn = document.getElementById('isbn')
-let subbtn = document.getElementById('submit')
+let btn = document.getElementById('submit')
+let tablebody = document.getElementById('book-list')
 
-let tableBody = document.getElementById('book-list')
-
-subbtn.addEventListener('click', function (e) {
+btn.addEventListener('click', function (e) {
 	e.preventDefault()
-	let Tvalue = title.value.trim()
-	let Avalue = author.value.trim()
-	let Ivalue = isbn.value.trim()
-
-	if(Tvalue == ""|| Avalue =="" || Ivalue ==""){
-		alert('please fill all the field')
-	}
-
+		let Tvalue = title.value.trim()
+		let Avalue = author.value.trim()
+		let Ivalue = isbn.value.trim()
 	let newRow = document.createElement('tr')
-	newRow.innerHTML = `<td>${Tvalue}</td>
-	                   <td>${Avalue}</td>
-					   <td>${Ivalue}</td>
-					   <td><button class = "delete">X</button></td>`
 
-	tableBody.append(newRow)
-
-
+		newRow.innerHTML = `<td>${Tvalue}</td>
+						   <td>${Avalue}</td>
+						   <td>${Ivalue}</td>
+						   <button class = "delete">X</button>`
+	tablebody.append(newRow)
+     author.value = ""
 	title.value = ""
-	author.value = ''
 	isbn.value = ""
+	let removeBtn = document.querySelector('.delete')
+	removeBtn.addEventListener('click', function () {
+		newRow.remove()
+		
+	})
 
-})
-
-tableBody.addEventListener('click', function (e) {
-	if(e.target.classList.contains('delete')){
-		let row = e.target.parentElement.parentElement
-		tableBody.removeChild(row)
-	}
+	
+	
 	
 })
